6f62590d91cbeec53f0fdc29bb87291fa4982fca
Adam Caron <adamwcaron@gmail.com>
2015-08-18 14:36:52 -0600

    Add a Contact page

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -22,4 +22,10 @@ class StaticPagesControllerTest < ActionController::TestCase
     assert_select "title", "About#{@title_suffix}"
   end
 
+  test "should get contact" do
+    get :contact
+    assert_response :success
+    assert_select "title", "Contact#{@title_suffix}"
+  end
+
 end
